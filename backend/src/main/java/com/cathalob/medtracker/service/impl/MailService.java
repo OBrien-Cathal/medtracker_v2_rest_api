8ec7e51312f69ba06cd42066078c583f06a0737f
OBrien-Cathal <obrien.cathal@proton.me>
2025-02-23 18:21:09 +0100

    removed mail test controller and related MailService function

--- backend/src/main/java/com/cathalob/medtracker/service/impl/MailService.java
+++ backend/src/main/java/com/cathalob/medtracker/service/impl/MailService.java
@@ -1,6 +1,5 @@
 package com.cathalob.medtracker.service.impl;
 
-import com.cathalob.medtracker.model.UserModel;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,17 +22,6 @@ public class MailService {
         this.javaMailSender = javaMailSender;
     }
 
-
-    public void sendEmail(UserModel user) throws MailException {
-
-        SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setFrom(fromAddress());
-        mail.setTo(user.getUsername());
-        mail.setSubject("MedTracker Notification");
-        mail.setText("Please confirm your registration by clicking on this link\n" +
-                "http.....");
-        javaMailSender.send(mail);
-    }
 
     public void sendEmail(String toAddress, String subject, String text) throws MailException {
 
